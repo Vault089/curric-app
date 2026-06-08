4552c0a73db8daddd2fa6db7702cc28067696b7c
Amro Badr <Amrbadr15@gmail.com>
2026-06-09 02:02:27 +0700

    Fix env.js - add NEXT_PUBLIC_APP_URL

--- env.js
+++ env.js
@@ -5,8 +5,11 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
   },
-  client: {},
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
+  },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   }
 });
