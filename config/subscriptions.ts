a1d182b26e9048b31bd36803c8029133da8f23ed
Amro Badr <Amrbadr15@gmail.com>
2026-06-09 01:16:27 +0700

    Remove Stripe from env validation - build fix

--- config/subscriptions.ts
+++ config/subscriptions.ts
@@ -1,6 +1,5 @@
 // @ts-nocheck
 import { SubscriptionPlan } from 'types';
-import { env } from '@/env.mjs';
 
 export const freePlan: SubscriptionPlan = {
   name: 'Free',
@@ -12,5 +11,5 @@ export const freePlan: SubscriptionPlan = {
 export const proPlan: SubscriptionPlan = {
   name: 'PRO',
   description: 'The PRO plan has unlimited posts.',
-  stripe_price_id: env.STRIPE_PRO_MONTHLY_PLAN_ID || ''
+  stripe_price_id: ''
 };
